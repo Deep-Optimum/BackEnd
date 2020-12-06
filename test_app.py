00c08b172225d33e5a9480b0c3870a27f38934ab
benningtonli <bennington.lee@gmail.com>
2020-12-06 13:21:28 -0500

    Upadded test_app.py to pytest

--- test_app.py
+++ test_app.py
@@ -1,150 +1,142 @@
-# import unittest
-# import json
-# import requests
-#
-#
-# class TestApp(unittest.TestCase):
+import pytest
+import json
+import app
 
 
-#     # @unittest.skip("Skip for now")
-#     def test_search(self):
-#         isbn_url = 'http://127.0.0.1:5000/books?isbn=9780072970548'
-#         rsp = requests.get(isbn_url)
-#         self.assertEqual(rsp.status_code, 200)
-#         self.assertEqual(rsp.json()[0]["isbn"], '9780072970548')
-#         title_url = 'http://127.0.0.1:5000/books?title=introduction%20to%20algorithms'
-#         rsp = requests.get(title_url)
-#         self.assertEqual(rsp.status_code, 200)
-#         self.assertEqual(rsp.json()[0]["title"], 'introduction to algorithms')
-#         subject_url = 'http://127.0.0.1:5000/books?subject=computer%20science'
-#         rsp = requests.get(subject_url)
-#         self.assertEqual(rsp.status_code, 200)
-#         self.assertEqual(rsp.json()[0]["category"], 'computer science')
-#
-#     # @unittest.skip("Skip for now")
-#     def test_create_new_post(self):
-#         url = 'http://127.0.0.1:5000/posts'
-#         payload = {"listing_id": "1", "isbn": "9780072970548", "uni": "yz3781", "title": "introduction to algorithms",
-#                    "price": 100.00, "category": "computer science", "description": "", "image_url": "", "is_sold": 0}
-#         rsp = requests.post(url, data=json.dumps(payload, indent=4))
-#         print(rsp.content)
-#         self.assertEqual(rsp.status_code, 200)
-#
-#     # @unittest.skip("Skip for now")
-#     def test_post_by_id(self):
-#         url = 'http://127.0.0.1:5000/posts/1'
-#         payload = {"listing_id": "1", "isbn": "9780072970548", "uni": "yz3781", "title": "introduction to algorithms",
-#                    "price": 110.00, "category": "computer science", "description": "", "image_url": "", "is_sold": 0}
-#         rsp_put = requests.put(url, data=json.dumps(payload, indent=4))
-#         self.assertEqual(rsp_put.status_code, 200)
-#         rsp_get = requests.get(url)
-#         self.assertEqual(rsp_get.status_code, 200)
-#         # self.assertEqual(rsp_get.json()[0]["listing_id"], '1')
-#         # rsp_del = requests.delete(url)
-#         # self.assertEqual(rsp_del.status_code, 200)
-#
-#     # @unittest.skip("Skip for now")
-#     def test_create_users(self):
-#         url = 'http://127.0.0.1:5000/users'
-#         payload = {"uni": "yz3781", "user_name": "Dennis", "email": "yz3710@columbia.edu",
-#                    "phone_number": "", "credential": ""}
-#         rsp = requests.post(url, data=json.dumps(payload, indent=4))
-#         self.assertEqual(rsp.status_code, 200)
-#
-#     # @unittest.skip("Skip for now")
-#     def test_get_user_posts(self):
-#         url = 'http://127.0.0.1:5000/users/yz3781/listings'
-#         rsp = requests.get(url)
-#         self.assertEqual(rsp.status_code, 200)
-#         self.assertEqual(rsp.json()[0]["uni"], 'yz3781')
-#
-#     # @unittest.skip("Skip for now")
-#     def test_user_by_uni(self):
-#         url = 'http://127.0.0.1:5000/users/yz3781'
-#         payload = {"uni": "yz3781", "user_name": "Dennis", "email": "yz3710@columbia.edu",
-#                    "phone_number": "1112223344", "credential": ""}
-#         rsp_put = requests.put(url, data=json.dumps(payload, indent=4))
-#         self.assertEqual(rsp_put.status_code, 200)
-#         rsp_get = requests.get(url)
-#         self.assertEqual(rsp_get.status_code, 200)
-#         self.assertEqual(rsp_get.json()[0]["uni"], 'yz3781')
-#
-#     # @unittest.skip("Skip for now")
-#     def test_user_address(self):
-#         url = 'http://127.0.0.1:5000/users/yz3781/addresses'
-#         rsp_get = requests.get(url)
-#         self.assertEqual(rsp_get.status_code, 200)
-#         self.assertEqual(rsp_get.json()[0]["uni"], 'yz3781')
-#
-#     # @unittest.skip("Skip for now")
-#     def test_user_orders(self):
-#         url = 'http://127.0.0.1:5000/users/yz3781/orders'
-#         rsp_get = requests.get(url)
-#         self.assertEqual(rsp_get.status_code, 200)
-#         self.assertEqual(rsp_get.json()[0]["uni"], 'yz3781')
-#
-#     # @unittest.skip("Skip for now")
-#     def test_create_address(self):
-#         url = 'http://127.0.0.1:5000/addresses'
-#         payload = {"address_id": "1", "uni": "yz3781", "country": "United States", "state": "NY", "city": "New York",
-#                    "address": "xx", "zipcode": "10025"}
-#         rsp = requests.post(url, data=json.dumps(payload, indent=4))
-#         self.assertEqual(rsp.status_code, 200)
-#     # @unittest.skip("Skip for now")
-#     def test_address_by_id(self):
-#         url = 'http://127.0.0.1:5000/addresses/1'
-#         payload = {"address_id": "1", "uni": "yz3781", "country": "United States", "state": "NY", "city": "New York",
-#                    "address": "xx street", "zipcode": "10025"}
-#         rsp_put = requests.put(url, data=json.dumps(payload, indent=4))
-#         rsp_del = requests.delete(url)
-#         self.assertEqual(rsp_del.status_code, 200)
-#         self.assertEqual(rsp_put.status_code, 200)
-#     # @unittest.skip("Skip for now")
-#     def test_create_order(self):
-#         url = 'http://127.0.0.1:5000/orders'
-#         payload = {"order_id": "1", "buyer_uni": "yz3781", "seller_uni": "ab1234", "listing_id": "2",
-#                    "transaction_amt": 78.00, "status": "In progress"}
-#         rsp = requests.post(url, data=json.dumps(payload, indent=4))
-#         self.assertEqual(rsp.status_code, 200)
-#     # @unittest.skip("Skip for now")
-#     def test_order_by_id(self):
-#         url = 'http://127.0.0.1:5000/orders/1'
-#         payload = {"order_id": "1", "buyer_uni": "yz3781", "seller_uni": "ab1234", "listing_id": "2",
-#                    "transaction_amt": 78.00, "status": "Completed"}
-#         rsp_put = requests.put(url, data=json.dumps(payload, indent=4))
-#         self.assertEqual(rsp_put.status_code, 200)
-#         rsp_get = requests.get(url)
-#         self.assertEqual(rsp_get.status_code, 200)
-#         # self.assertEqual(rsp_get.json()[0]["order_id"], '1')
-#         # rsp_del = requests.delete(url)
-#         # self.assertEqual(rsp_del.status_code, 200)
-#
-        #def test_create_checkout(self):
-        #    self.
-#
-# if __name__ == '__main__':
-#     t = TestApp()
-#     t.test_search()
-#     t.test_create_new_post()
-#     t.test_post_by_id()
-#     t.test_create_users()
-#     t.test_get_user_posts()
-#     t.test_user_by_uni()
-#     t.test_user_address()
-#     t.test_user_orders()
-#     t.test_create_address()
-#     t.test_address_by_id()
-#     t.test_create_order()
-#     t.test_order_by_id()
-#     # t.test_search()
-#     # t.test_create_users()
-#     # t.test_create_address()
-#     # t.test_address_by_id()
-#     # t.test_create_order()
-#     # t.test_order_by_id()
-#     # t.test_get_user_posts()
-#     # t.test_create_new_post()
-#     # t.test_post_by_id()
-#     # t.test_user_by_uni()
-#     # t.test_user_address()
-#     # t.test_user_orders()
+@pytest.fixture
+def client():
+    app.app.config['TESTING'] = True
+    with app.app.test_client() as client:
+        yield client
+
+
+@pytest.mark.order(4)
+def test_search(client):
+    isbn_url = 'http://127.0.0.1:5000/books?isbn=9780072970548'
+    rsp = client.get(isbn_url)
+    assert rsp.status_code == 200
+    data = json.loads(rsp.data)
+    assert data[0]["isbn"] == '9780072970548'
+    title_url = 'http://127.0.0.1:5000/books?title=introduction%20to%20algorithms'
+    rsp = client.get(title_url)
+    assert rsp.status_code == 200
+    data = json.loads(rsp.data)
+    assert data[0]["title"] == 'introduction to algorithms'
+    subject_url = 'http://127.0.0.1:5000/books?subject=computer%20science'
+    rsp = client.get(subject_url)
+    assert rsp.status_code == 200
+    data = json.loads(rsp.data)
+    assert data[0]["category"] == 'computer science'
+
+
+@pytest.mark.order(1)
+def test_create_new_post(client):
+    url = 'http://127.0.0.1:5000/posts'
+    payload = {"listing_id": "1", "isbn": "9780072970548", "uni": "yz3781", "title": "introduction to algorithms",
+               "price": 100.00, "category": "computer science", "description": "", "image_url": "", "is_sold": 0}
+    rsp = client.post(url, data=json.dumps(payload, indent=4))
+    assert rsp.status_code == 200
+
+
+def test_post_by_id(client):
+    url = 'http://127.0.0.1:5000/posts/1'
+    payload = {"listing_id": "1", "isbn": "9780072970548", "uni": "yz3781", "title": "introduction to algorithms",
+               "price": 110.00, "category": "computer science", "description": "", "image_url": "", "is_sold": 0}
+    rsp_put = client.put(url, data=json.dumps(payload, indent=4))
+    assert rsp_put.status_code == 200
+    rsp_get = client.get(url)
+    assert rsp_get.status_code == 200
+    data = json.loads(rsp_get.data)
+    assert data[0]["listing_id"] == '1'
+    rsp_del = client.delete(url)
+    assert rsp_del.status_code == 200
+
+
+@pytest.mark.order(2)
+def test_create_users(client):
+    url = 'http://127.0.0.1:5000/users'
+    payload = {"uni": "yz3781", "user_name": "Dennis", "email": "yz3710@columbia.edu",
+               "phone_number": "", "credential": ""}
+    rsp = client.post(url, data=json.dumps(payload, indent=4))
+    assert rsp.status_code == 200
+
+
+@pytest.mark.order(5)
+def test_get_user_posts(client):
+    url = 'http://127.0.0.1:5000/users/yz3781/listings'
+    rsp = client.get(url)
+    assert rsp.status_code == 200
+    data = json.loads(rsp.data)
+    assert data[0]["uni"] == 'yz3781'
+
+
+def test_user_by_uni(client):
+    url = 'http://127.0.0.1:5000/users/yz3781'
+    payload = {"uni": "yz3781", "user_name": "Dennis", "email": "yz3710@columbia.edu",
+               "phone_number": "1112223344", "credential": ""}
+    rsp_put = client.put(url, data=json.dumps(payload, indent=4))
+    assert rsp_put.status_code == 200
+    rsp_get = client.get(url)
+    assert rsp_get.status_code == 200
+    data = json.loads(rsp_get.data)
+    assert data[0]["uni"] == 'yz3781'
+
+
+@pytest.mark.order(6)
+def test_user_address(client):
+    url = 'http://127.0.0.1:5000/users/yz3781/addresses'
+    rsp = client.get(url)
+    assert rsp.status_code == 200
+    data = json.loads(rsp.data)
+    assert data[0]["uni"] == 'yz3781'
+
+
+@pytest.mark.order(7)
+def test_user_orders(client):
+    url = 'http://127.0.0.1:5000/users/yz3781/orders'
+    rsp = client.get(url)
+    assert rsp.status_code == 200
+    data = json.loads(rsp.data)
+    assert data[0]["buyer_uni"] == 'yz3781'
+
+
+@pytest.mark.order(3)
+def test_create_address(client):
+    url = 'http://127.0.0.1:5000/addresses'
+    payload = {"address_id": "1", "uni": "yz3781", "country": "United States", "state": "NY", "city": "New York",
+               "address": "xx", "zipcode": "10025"}
+    rsp = client.post(url, data=json.dumps(payload, indent=4))
+    assert rsp.status_code == 200
+
+
+def test_address_by_id(client):
+    url = 'http://127.0.0.1:5000/addresses/1'
+    payload = {"address_id": "1", "uni": "yz3781", "country": "United States", "state": "NY", "city": "New York",
+               "address": "xx street", "zipcode": "10025"}
+    rsp_put = client.put(url, data=json.dumps(payload, indent=4))
+    assert rsp_put.status_code == 200
+    rsp_del = client.delete(url)
+    assert rsp_del.status_code == 200
+
+
+@pytest.mark.order(4)
+def test_create_order(client):
+    url = 'http://127.0.0.1:5000/orders'
+    payload = {"order_id": "1", "buyer_uni": "yz3781", "seller_uni": "ab1234", "listing_id": "2",
+               "transaction_amt": 78.00, "status": "In progress"}
+    rsp = client.post(url, data=json.dumps(payload, indent=4))
+    assert rsp.status_code == 200
+
+
+def test_order_by_id(client):
+    url = 'http://127.0.0.1:5000/orders/1'
+    payload = {"order_id": "1", "buyer_uni": "yz3781", "seller_uni": "ab1234", "listing_id": "2",
+               "transaction_amt": 78.00, "status": "Completed"}
+    rsp_put = client.put(url, data=json.dumps(payload, indent=4))
+    assert rsp_put.status_code == 200
+    rsp_get = client.get(url)
+    assert rsp_get.status_code == 200
+    data = json.loads(rsp_get.data)
+    assert data[0]["order_id"] == '1'
+    rsp_del = client.delete(url)
+    assert rsp_del.status_code == 200
