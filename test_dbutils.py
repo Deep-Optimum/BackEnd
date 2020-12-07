aa3008b28d1cdfb2f9164708775f96377cafaf97
benningtonli <bennington.lee@gmail.com>
2020-12-06 20:13:13 -0500

    Updated backend model for pattern match search.

--- test_dbutils.py
+++ test_dbutils.py
@@ -56,6 +56,43 @@ def test_template_to_where_clause():
     expected = (' WHERE  address_id=%s AND uni=%s ', ['2', 'wl2750'])
     assert actual == expected
 
+def test_template_to_where_clause_no_template():
+    template = {}
+    actual = dbutils.template_to_where_clause(template)
+    expected = ("", None)
+    assert actual == expected
+    actual = dbutils.template_to_where_clause(None)
+    expected = ("", None)
+    assert actual == expected
+
+def test_template_to_where_clause_1key_n_vals():
+    template = {"title": ["%computer%vision%", "%vision%", "%computer%"]}
+    actual = dbutils.template_to_where_clause(template, is_like=True, is_or=True)
+    expected = (' WHERE  title LIKE %s OR title LIKE %s OR title LIKE %s ', ["%computer%vision%", "%vision%", "%computer%"])
+    assert actual == expected
+
+    actual = dbutils.template_to_where_clause(template, is_like=True)
+    expected = (' WHERE  title LIKE %s AND title LIKE %s AND title LIKE %s ', ["%computer%vision%", "%vision%", "%computer%"])
+    assert actual == expected
+
+def test_template_to_where_clause_nkeys_n_vals():
+    template = {"title": ["%computer%vision%", "%vision%", "%computer%"], "isbn": ["%12%", "%34%"]}
+    actual = dbutils.template_to_where_clause(template, is_like=True, is_or=True)
+    expected = (' WHERE  title LIKE %s OR title LIKE %s OR title LIKE %s OR isbn LIKE %s OR isbn LIKE %s ',
+                ["%computer%vision%", "%vision%", "%computer%", "%12%", "%34%"])
+    assert actual == expected
+
+    actual = dbutils.template_to_where_clause(template, is_like=True)
+    expected = (' WHERE  title LIKE %s AND title LIKE %s AND title LIKE %s AND isbn LIKE %s AND isbn LIKE %s ',
+                ["%computer%vision%", "%vision%", "%computer%", "%12%", "%34%"])
+    assert actual == expected
+
+    template = {"title": ["%computer%vision%", "%vision%", "%computer%"], "isbn": "%34%"}
+    actual = dbutils.template_to_where_clause(template, is_like=True)
+    expected = (' WHERE  title LIKE %s AND title LIKE %s AND title LIKE %s AND isbn LIKE %s ',
+                ["%computer%vision%", "%vision%", "%computer%", "%34%"])
+    assert actual == expected
+
 def test_template_to_where_clause_is_like():
     template = {"uni": "%2%", "email": '%columbia%'}
     actual = dbutils.template_to_where_clause(template, is_like=True)
@@ -68,6 +105,12 @@ def test_create_select():
     expected = ('select  *  from Addresses  WHERE  address_id=%s AND uni=%s ', ['2', 'wl2750'])
     assert actual == expected
 
+def test_create_select_single():
+    template = {"address_id": "2"}
+    actual = dbutils.create_select("Addresses", template, is_select=True)
+    expected = ('select  *  from Addresses  WHERE  address_id=%s ', ['2'])
+    assert actual == expected
+
 def test_create_select_w_field_list():
     template = {"address_id": "2", "uni": "wl2750"}
     fields = ["address_id", "state"]
@@ -75,6 +118,26 @@ def test_create_select_w_field_list():
     expected = ('select  address_id,state  from Addresses  WHERE  address_id=%s AND uni=%s ', ['2', 'wl2750'])
     assert actual == expected
 
+def test_create_select_w_field_list_single():
+    template = {"address_id": "2"}
+    fields = ["address_id", "state"]
+    actual = dbutils.create_select("Addresses", template, fields=fields, is_select=True)
+    expected = ('select  address_id,state  from Addresses  WHERE  address_id=%s ', ['2'])
+    assert actual == expected
+
+def test_create_select_is_like_order_by():
+    template = {"title": ["%computer%vision%", "%vision%", "%computer%"]}
+    actual = dbutils.create_select("Listings", template, order_by=['category'], is_select=True, is_like=True, is_or=True)
+    expected = ('select  *  from Listings  WHERE  title LIKE %s OR title LIKE %s OR title '
+                'LIKE %s order by category', ['%computer%vision%', '%vision%', '%computer%'])
+    assert actual == expected
+
+def test_create_select_is_like_limit_by():
+    template = {"title": ["%computer%vision%", "%vision%", "%computer%"]}
+    actual = dbutils.create_select("Listings", template, order_by=['category'], limit=10, is_select=True, is_like=True)
+    expected = ('select  *  from Listings  WHERE  title LIKE %s AND title LIKE %s AND title '
+                'LIKE %s order by category limit 10',['%computer%vision%', '%vision%', '%computer%'])
+    assert actual == expected
 
 def test_create_delete():
     template = {"address_id": "2", "uni": "wl2750"}
@@ -92,5 +155,18 @@ def test_create_update():
 def test_create_select_with_like():
     template = {"uni": "%2%", "email": '%columbia%'}
     actual = dbutils.create_select("User_info", template, is_select=True, is_like=True)
-    expected = ('select  *  from User_info  WHERE  uni LIKE %s AND email LIKE %s ',  ['%2%', '%columbia%'])
+    expected = ('select  *  from User_info  WHERE  uni LIKE %s AND email LIKE %s ',['%2%', '%columbia%'])
+    assert actual == expected
+
+def test_create_select_with_like_single_field():
+    template = {"uni": "%2%"}
+    actual = dbutils.create_select("User_info", template, is_select=True, is_like=True)
+    expected = ('select  *  from User_info  WHERE  uni LIKE %s ', ['%2%'])
+    assert actual == expected
+
+def test_create_select_with_like_three_fields():
+    template = {"uni": "%2%", "email": '%columbia%', "credential": "k" }
+    actual = dbutils.create_select("User_info", template, is_select=True, is_like=True)
+    expected = ('select  *  from User_info  WHERE  uni LIKE %s AND email LIKE %s AND '
+                'credential LIKE %s ', ['%2%', '%columbia%', 'k'])
     assert actual == expected
